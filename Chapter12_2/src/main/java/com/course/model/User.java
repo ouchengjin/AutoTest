a5df0416e136c9c047b4079191c648614363c41a
ouchengjin <953816929@qq.com>
2020-12-15 22:48:51 +0800

    维护

--- Chapter12_2/src/main/java/com/course/model/User.java
+++ Chapter12_2/src/main/java/com/course/model/User.java
@@ -11,30 +11,4 @@ public class User {
     private Integer sex;
     private Integer permission;
     private Integer isDelete;
-
-    public User() {
-    }
-
-    public User(Integer id,String userName, String password, Integer age, Integer sex, Integer permission, Integer isDelete) {
-        this.id = id;
-        this.userName = userName;
-        this.password = password;
-        this.age = age;
-        this.sex = sex;
-        this.permission = permission;
-        this.isDelete = isDelete;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", userName='" + userName + '\'' +
-                ", password='" + password + '\'' +
-                ", age=" + age +
-                ", sex=" + sex +
-                ", permission=" + permission +
-                ", isDelete=" + isDelete +
-                '}';
-    }
 }
